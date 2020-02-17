267b31f353e41dea6dc616a89ef07f259b79b397
Matthew John Ragas <mjrag310@hotmail.com>
2020-02-17 17:42:25 -0600

    finished iterator and list class

--- raygame/Iterator.h
+++ raygame/Iterator.h
@@ -18,42 +18,49 @@ private:
 	Node* current;
 };
 
+//Default constructor
 template<typename T>
 Iterator<T>::Iterator()
 {
 
 }
 
+//custom constructor
 template<typename T>
 Iterator<T>::Iterator(Node<T>* ptr)
 {
 	current = ptr;
 }
 
+//overloading the '*' operator
 template<typename T>
 T Iterator<T>::operator*()
 {
 	return current->info;
 }
 
+//overloading the addition operator "++"
 template<typename T>
 Iterator<T> Iterator<T>::operator++()
 {
 	return current = current->next;
 }
 
+//overloading the subtraction operator "--"
 template<typename T>
 Iterator<T> Iterator<T>::operator--()
 {
 	current = current->previous;
 }
 
+//overloading the equals operator "=="
 template<typename T>
 bool Iterator<T>::operator==(const Iterator<T>& iter)const
 {
 	return current == iter.current;
 }
 
+//overloading the not equals operator "!="
 template<typename T>
 bool Iterator<T>::operator!=(const Iterator<T>& iter)const
 {
--- raygame/List.h
+++ raygame/List.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "Node.h"
+#include"Iterator.h"
 
 template<typename T>
 class List
 {
 protected:
+
+	//length of list
 	int mCount;
 	Node<T>* first;
 	Node<T>* last;
@@ -15,24 +18,101 @@ public:
 	int Length();
 	T front();
 	T back();
-	virtual bool search(const T&) = 0;
-	virtual void insertFirst(const T&) = 0;
-	virtual void insertLast(const T&) = 0;
-	virtual void deleteNode(const T&) = 0;
-	iterator<T> begin();
-	iterator<T> end();
+	virtual bool search(const T& iter) = 0;
+	virtual void insertFirst(const T& iter) = 0;
+	virtual void insertLast(const T& iter) = 0;
+	virtual void deleteNode(const T& iter) = 0;
+	Iterator<T> begin();
+	Iterator<T> end();
 	List();
-	List(List<T>&);
+	List(List<T>& list);
 	~List();
 
 private:
 	void copyList(List<T>&);
 };
 
+//Initializes the list
 template<typename T>
 void List<T>::initializeList()
 {
 	mCoutn = 0;
 	first = last;
 	last = nullptr;
+}
+
+//returns if the list is empty
+template<typename T>
+bool List<T>::isEmptyList()
+{
+	if (mCount == 0)
+		return true;
+	else
+		return false;
+}
+
+//returns the length of the list
+template<typename T>
+int List<T>::Length()
+{
+	return mCount;
+}
+
+//
+template<typename T>
+T List<T>::front()
+{
+	return first;
+}
+
+//
+template<typename T>
+T List<T>::back()
+{
+	return last;
+}
+
+//
+template<typename T>
+Iterator<T> List<T>::begin()
+{
+	Iterator<T> iter = new Iterator<T>(first);
+	return iter;
+}
+
+//
+template<typename T>
+Iterator<T> List<T>::end()
+{
+	Iterator<T> iter = new Iterator(last);
+	return iter;
+}
+
+//default constructor
+template<typename T>
+List<T>::List()
+{
+
+}
+
+//custom constructor
+template<typename T>
+List<T>::List(List<T>& list)
+{
+
+}
+
+//Deconstructor
+template<typename T>
+List<T>::~List()
+{
+
+}
+
+//copy the list
+template<typename T>
+void List<T>::copyList(List<T>& list)
+{
+	Node<T>* iter = &(*this->first);
+	Node<T>* iter2 = &(*other.first);
 }
